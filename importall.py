f8ec93a6073af0556f3385088e8d56a9e5b7eead
ahmedsaalah <a7mad.sala7@live.com>
2018-11-27 16:20:44 +0200

    Update importall.py

--- importall.py
+++ importall.py
@@ -9,6 +9,6 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import sessionmaker
 from datetime import datetime
 app =Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:kramara1234@localhost/ouma'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:kramara1234@138.197.185.15/ouma'
 
 db = SQLAlchemy(app)
