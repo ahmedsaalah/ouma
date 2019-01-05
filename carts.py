f781f1202a233130ea1232c7186afea24b9ee4ac
salah <a7mad.sala7@live.com>
2019-01-05 18:45:09 +0200

    WEDdd

--- carts.py
+++ carts.py
@@ -4,14 +4,16 @@ class cart(db.Model):
    id = db.Column('cart_id', db.Integer, primary_key = True)
    product_id = db.Column(db.Integer, db.ForeignKey('product.product_id'),nullable=False)
    order_id = db.Column(db.Integer, db.ForeignKey('order.order_id'),nullable=False)
-   occur = db.Column(db.Integer) 
+   occur = db.Column(db.Integer)
+   notes=  db.Column(db.String(3024))  
 
 
 
-def __init__(self, product_id, order_id,occur):
+def __init__(self, product_id, order_id,occur,notes):
    self.product_id = product_id
    self.order_id = order_id
    self.occur = occur
+   self.notes = notes
 
 
 
